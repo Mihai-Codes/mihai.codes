40b20f9d82fca64260abe552234328dfa32cf5e6
Chindriș Mihai - Alexandru <mc146@students.quantic.edu>
2026-04-15 22:01:56 +0300

    Add second MongoDB Credly badge to credentials

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -8,6 +8,7 @@ const credlyBadgeIds = [
   '221c7861-b767-4de7-8c35-cdbed40cf16b',
   'a1afd0f1-8f0b-4c0a-966c-8c04462e0e3e',
   '87fb16e1-7370-46e2-9f5c-c8f5320fdc23',
+  '720caef7-fa18-4dd4-bee8-579cf666de2a',
 ];
 
 // Fetch Credly badge data at build time (SSG) or request time (SSR)
